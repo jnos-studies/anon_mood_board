9ad22de47aaa4f02d85bbfc7a8a362924439dfe5
writatas <jnelson.writing@gmail.com>
2022-08-15 17:12:25 +0200

    setup

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from werkzueg.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from authentication import apology, login_required
+
+from datetime import datetime as date
+
+import re
+
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_PERMANENT"] = False
+Session(app)
+
