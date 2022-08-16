652e6c1dfca375a5ba513088552718142077df63
writatas <jnelson.writing@gmail.com>
2022-08-16 10:06:31 +0200

    setup databse

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from cs50 import SQL
 from authentication import apology, login_required
 
 from datetime import datetime as date
@@ -16,3 +16,4 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 Session(app)
 
+db = SQL(os.environ.get("DATABASE_URL"))
