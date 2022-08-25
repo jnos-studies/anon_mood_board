7d5a4c345ca1ff3b891fd8fb83cc5b968a33e37f
writatas <jnelson.writing@gmail.com>
2022-08-25 09:52:40 +0200

    setup database and added another module to generate images which will reflect user's overall mood

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
 
-from free_apis import get_random_quote
+from free_apis import get_random_quote as quote
+
 from authentication import apology, login_required
 
 from datetime import datetime as date
@@ -18,4 +19,4 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 Session(app)
 
-db = SQL(os.environ.get("DATABASE_URL"))
+db = SQL("sqlite:///test_data.db")
---
+++ image_generator.py
@@ -0,0 +1,5 @@
+from PIL import Image
+# https://holypython.com/python-pil-tutorial/how-to-create-a-new-image-with-pil/
+import os
+
+
