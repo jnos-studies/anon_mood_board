ebfea75963902ed37a467d4f50c9561caac684db
writatas <jnelson.writing@gmail.com>
2022-08-25 16:33:00 +0200

    Added an image making function for user icons, the names of which will be random so one does not know which user has which image without access to the database, working on app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
 
 from free_apis import get_random_quote as quote
-
+from image_generator import make_mood_image
 from authentication import apology, login_required
 
 from datetime import datetime as date
@@ -15,8 +15,23 @@ import re
 
 app = Flask(__name__)
 
+# configure session to use filesystem (instead of signed cookies)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 Session(app)
 
+# Testing database to be used before production database
 db = SQL("sqlite:///test_data.db")
+
+@app.after_request
+def after_request(response):
+    # Ensure responses are not cached
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    """Show user's homepage"""
--- image_generator.py
+++ image_generator.py
@@ -1,5 +1,9 @@
 from PIL import Image
 # https://holypython.com/python-pil-tutorial/how-to-create-a-new-image-with-pil/
-import os
 
+def make_mood_image (name, avg):
+    new = Image.new(mode="RGB", size=(32,32), color=(avg[0], avg[1], avg[2]))
+    new.save(f"static/mood_images/{name}.png")
+
+make_mood_image("test",50)
 
