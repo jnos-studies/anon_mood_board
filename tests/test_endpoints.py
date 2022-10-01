9de77dca12014e8750cf950e1d8d339ddc65f54e
writatas <jnelson.writing@gmail.com>
2022-10-01 10:05:20 +0200

    working on making a test that checks endpoints

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,4 +1,20 @@
 import requests
-import flask
 import os
+import signal
+import subprocess
 
+def test_endpoints():
+    # Open a test process using localhost to test the endpoints
+    open_test = subprocess.Popen(["flask", "run --debug -h localhost -p 5550"],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True)
+
+    stdout, stderr = open_test.communicate()
+    stdout, stderr
+
+    
+    print("flask testing app running on port 5550, pid {}".format(open_test.pid))
+    # Send a signal terminate to all process groups and close the testing
+    
+test_endpoints()
