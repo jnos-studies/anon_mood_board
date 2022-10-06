0b78dac0e809d8249d96ad955f15e995a324f4d5
writatas <jnelson.writing@gmail.com>
2022-10-06 09:33:19 +0200

    Configured testing environment to run my unit test with a single command line. In the future I will alias this as test, but for now the command runs as follows, bash refresh.sh [True/False]. True means to perform tests, False would mean to start as production

--- refresh.sh
+++ refresh.sh
@@ -1,13 +1,16 @@
-rm -r app/static/mood_images/*
-rm -r app/static/word_maps/*
-rm -f test_data.db
-sqlite3 test_data.db < .database_schema.sql
-rm -r flask_session
-
 if echo $1 == "True"
 then
     export FLASK_APP="app.main:create_app(test=True)"
-    flask run
+    rm -r app/static/mood_images/*
+    rm -r app/static/word_maps/*
+    rm -f test_data.db
+    sqlite3 test_data.db < .database_schema.sql
+    rm -r flask_session
+
+    flask run &
+    sleep 5; pytest > tests/test_output.txt
+    fuser -k 5000/tcp
+    printf "\n\nTest completed! see tests/test_output.txt"
 else
     export FLASK_APP="app.main:create_app(test=False)"
     flask run
